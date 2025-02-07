eb3016a1a84d9040187321c83dfff130e0328f9d
YukeyHo <yukeyho@163.com>
2025-02-07 19:10:47 +0800

    Create cocktile_sort.py
    鸡尾酒排序算法（双向冒泡排序）。它的时间复杂度仍然是O(n^2)，只是减少了一些无效比较。只适用于输入数据量很小的情况。

---
+++ 排序算法/cocktile_sort.py
@@ -0,0 +1,21 @@
+def cocktail_sort(arr):
+    n = len(arr)
+    left, right = 0, n - 1
+    while left < right:
+        swapped = False
+        for i in range(left, right):  # 从左到右冒泡
+            if arr[i] > arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                swapped = True
+        right -= 1  # 右边界缩小
+        for i in range(right, left, -1):  # 从右到左冒泡
+            if arr[i] < arr[i - 1]:
+                arr[i], arr[i - 1] = arr[i - 1], arr[i]
+                swapped = True
+        left += 1  # 左边界缩小
+        if not swapped:
+            break  # 如果没有交换，提前结束
+    return arr
+
+#鸡尾酒排序（Cocktile Sort）算法，因为其执行过程像是有一个调酒师在左右摇晃调酒壶（Shaker）而得名。
+#你可以想象在一个调酒壶中，大的气泡往右边冒泡，小的气泡往左边冒泡，由此完成了气泡从小到大的升序排列。
